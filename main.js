25328197099aeda0e45ab9274940c85803ec1719
BogdanHomyak <homa1406c@gmail.com>
2024-06-15 23:07:34 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,40 @@
+// 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+let a = 'hello';
+console.log(typeof a, a);
+let b = "owu";
+console.log(typeof b, b);
+let c = `com`;
+console.log(typeof c, c);
+let d = 'ua';
+console.log(typeof d, d);
+let e = 1;
+console.log(typeof e, e);
+let f = 10;
+console.log(typeof f, f);
+let g = -999;
+console.log(g);
+let h = 123;
+console.log(h);
+let k = 3.14;
+console.log(k);
+let l = 2.7;
+console.log(l);
+let m = 16;
+console.log(m);
+let n = true;
+console.log(n);
+let x = false;
+console.log(x);
+
+
+let firstName = 'Bogdan';
+let middleName = 'Homyak';
+let lastName = 'Igorovich';
+
+let person = firstName + ' ' + middleName + ' ' + lastName;
+console.log(person);
+
+let p1 = prompt("Введіть своє ім'я" );
+let p2 = prompt("Введіть своє Прізвище");
+let p3 = prompt("Введіть своє по-батькові");
+console.log(`${p1} ${p2} ${p3}`)
